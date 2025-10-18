8ce39bd68dbb591eeea775dd9b26523d663088a9
f2025-0843-rgb <f2025-0843@bnu.edu.pk>
2025-10-18 13:35:44 +0500

    Add files via upload

---
+++ CLionProjects/main.cpp/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //task 1
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+    cout << "****" << endl;
+    cout << "*****" << endl;
+
+    string name = "Abdullah";
+    int age = 18;
+    string city = "Lahore";
+    cout << "My name is " << name << " , and i am" << age << "years old." <<endl;
+    cout << "I am  from" << city << "." << endl;
+
+    //Task 2
+
+
+
+    return 0;
+}
